6cb06b312cd80b723fceca24eb6aeef682e70503
Mike Crawford <106125495+devurandom11@users.noreply.github.com>
2023-10-19 10:06:05 -0700

    Update kali-setup.sh

--- kali-setup.sh
+++ kali-setup.sh
@@ -3,175 +3,141 @@
 # Function to check for necessary dependencies
 check_dependencies() {
     echo "Updating package list..."
-    sudo apt update
-    dpkg -l | grep -qw git || {
-        echo "Git is not installed. Installing Git..."
-        sudo apt install -y git
-    }
+    sudo apt update -q
+    dpkg -l | grep -qw git || sudo apt install -yq git
 }
 
 # Function to display help menu
 display_help() {
     echo "Usage: sudo $0 [option]"
-    echo
-    echo "This script provides a TUI menu for Kali Linux configurations."
-    echo "Run without arguments to access the TUI menu."
-    echo "Available options:"
-    echo "  -h, --help    Display this help menu and exit"
-    echo
-    echo "TUI Menu Options:"
-    echo "  U: Update Kali Linux"
-    echo "     - Updates the package list for upgrades and new package installations."
-    echo "     - Upgrades installed packages to their latest versions."
-    echo
-    echo "  I: Install kali-headless-tools"
-    echo "     - Installs headless tools for Kali Linux."
-    echo
-    echo "  D: Install desktop and Kali Linux default tools"
-    echo "     - Installs the default Kali Linux tools along with the desktop environment."
-    echo
-    echo "  A: Install desktop and all Kali Linux tools and pimpmykali"
-    echo "     - Installs all Kali Linux tools, the desktop environment, and runs pimpmykali for further customization."
-    echo
-    echo "  C: Change Repositories to Cloudflare"
-    echo "     - Changes the APT repositories to use Cloudflare mirrors for faster downloads."
-    echo
+    echo "Options: -h|--help"
     exit 0
 }
 
-# Check if -h or --help argument is passed
-if [[ $1 == "-h" || $1 == "--help" ]]; then
-    display_help
-fi
+# Check for help argument
+[[ $1 == "-h" || $1 == "--help" ]] && display_help
 
-# Ensure the script is run as root
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root. Rerun with sudo $0."
-    echo "Exiting."
+# Verify root access
+[[ $EUID -ne 0 ]] && {
+    echo "Run as root. Exiting."
     exit 1
-fi
-
-# Display TUI menu
-display_menu() {
-    while true; do
-        echo ""
-        echo ""
-        echo "Please select an option:"
-        echo "  U: Update Kali Linux"
-        echo "  I: Install kali-headless-tools"
-        echo "  D: Install desktop and Kali Linux default tools"
-        echo "  A: Install desktop and all Kali Linux tools and pimpmykali"
-        echo "  C: Change Repositories to Cloudflare"
-        echo "  Q: Quit"
-        echo ""
-        echo ""
-        read -p "Enter your choice: " choice
-        case $choice in
-        U | u)
-            update_kali
-            ;;
-        I | i)
-            install_headless
-            ;;
-        D | d)
-            install_desktop_default
-            ;;
-        A | a)
-            install_all_pimp
-            ;;
-        C | c)
-            change_repos
-            ;;
-        Q | q)
-            echo "Exiting."
-            exit 0
-            ;;
-        *)
-            echo "Invalid option $choice, please try again."
-            ;;
-        esac
-    done
 }
 
 # Function to change repositories to Cloudflare
 change_repos() {
-    echo "Changing repositories to Cloudflare..."
-    read -p "Are you sure you want to change the repositories? (y/N): " confirmation
-    if [[ $confirmation =~ ^[Yy]$ ]]; then
-        # Backup the original sources.list file
-        sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
-
-        # Replace the repositories in sources.list with Cloudflare repositories
-        sudo sed -i 's|http://http.kali.org/kali|http://kali.download/kali|g' /etc/apt/sources.list
-
-        echo "Repositories changed to Cloudflare. The original sources.list file has been backed up to /etc/apt/sources.list.bak."
-    else
-        echo "Operation cancelled by user."
-    fi
+    read -p "Switch repositories? (y/N): " confirmation
+    [[ $confirmation =~ ^[Yy]$ ]] || {
+        echo "Cancelled."
+        return
+    }
+    sudo cp /etc/apt/sources.list{,.bak}
+    sudo sed -i 's|http://http.kali.org/kali|http://kali.download/kali|g' /etc/apt/sources.list
 }
 
 # Function to update Kali Linux
 update_kali() {
-    echo "Updating Kali Linux..."
-    sudo apt update && sudo apt dist-upgrade -y && sudo apt autoremove -y
-    echo "Update completed."
+    sudo apt update -q && sudo apt dist-upgrade -yq && sudo apt autoremove -yq
+}
+
+# Function to clean up
+cleanup() {
+    apt clean && apt autoclean && apt autoremove -y
+    rm -rf /var/cache/apt/archives/*
 }
 
 # Function to install headless tools
 install_headless() {
-    echo "Installing kali-linux-headless..."
-    if ! sudo apt-get install -y kali-linux-headless; then
-        echo "Failed to install kali-linux-headless. Please check your network connection and try again."
+    update_kali
+    sudo apt install -yq kali-linux-headless kali-linux-firmware htop btop vim tldr ninja-build gettext cmake unzip curl cargo ripgrep gdu || {
+        echo "Network issue. Exiting."
         exit 1
+    }
+    su kali -c 'tldr -u'
+    if command -v nvim >/dev/null; then
+        echo "Neovim already installed. Skipping..."
+    else
+        install_neovim
     fi
-    echo "kali-linux-headless installation complete."
+    cleanup
+}
+
+# Function to install neovim
+install_neovim() {
+    [ -d "neovim" ] && echo "neovim exists. Skipping git clone..." || git clone https://github.com/neovim/neovim
+    cd neovim || {
+        echo "Failed to cd to neovim"
+        exit 1
+    }
+    git checkout stable
+
+    make CMAKE_BUILD_TYPE=RelWithDebInfo || {
+        echo "Failed to make neovim..."
+        exit 1
+    }
+    cd build && cpack -G DEB && dpkg -i nvim-linux64.deb
+    wget -q -O /tmp/CascadiaCode.zip "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip"
+    unzip -o -d ~/.fonts /tmp/CascadiaCode.zip
+    fc-cache -fv
+    rm /tmp/CascadiaCode.zip
+
+    cargo install tree-sitter-cli
+    curl -LO https://github.com/ClementTsang/bottom/releases/download/0.9.6/bottom_0.9.6_amd64.deb
+    sudo dpkg -i bottom_0.9.6_amd64.deb
+    mv ~/.config/nvim ~/.config/nvim.bak
+    mv ~/.local/share/nvim ~/.local/share/nvim.bak
+    mv ~/.local/state/nvim ~/.local/state/nvim.bak
+    mv ~/.cache/nvim ~/.cache/nvim.bak
+    git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+    cd /home/kali || return
+    rm -rf neovim
 }
 
 # Function to install Desktop
 install_desktop_default() {
-    echo "Installing Desktop Environment and enabling RDP..."
-    if ! sudo apt-get install -y kali-desktop-xfce kali-linux-default xrdp || ! sudo systemctl enable --now xrdp; then
-        echo "Failed to complete the installation or enable RDP. Please check your network connection and try again."
-        exit 1
-    fi
-    echo "Desktop Environment installed and RDP enabled successfully."
+    install_headless
+    sudo apt install -yq kali-desktop-xfce kali-linux-default kali-tools-* xrdp && sudo systemctl enable --now xrdp
+    cleanup
 }
 
 # Function to install all including pimp my kali
 install_all_pimp() {
-    echo "Installing All Tools and pimping Kali..."
-    echo ""
-    echo "Executing install_desktop_default function..."
-    install_desktop_default || {
-        echo "Failed executing install_desktop_default function."
+    install_desktop_default
+    [ -d "pimpmykali" ] && echo "pimpmykali exists. Skipping git clone..." || git clone https://github.com/Dewalt-arch/pimpmykali.git
+    cd pimpmykali || {
+        echo "Failed to cd to pimpmykali..."
         exit 1
     }
-    echo "install_desktop_default function executed successfully."
-    echo ""
-    echo "Installing kali-linux-large..."
-    sudo apt-get install -y kali-linux-large || {
-        echo "Failed to install kali-linux-large. Please check your network connection and try again."
+    ./pimpmykali.sh || {
+        echo "Failed to run pimpmykali.sh"
         exit 1
     }
-    echo "kali-linux-large installed successfully."
-    echo ""
-    echo "Cloning pimpmykali repository..."
-    git clone https://github.com/Dewalt-arch/pimpmykali.git || {
-        echo "Failed to clone pimpmykali repository. Please check your network connection and try again."
-        exit 1
-    }
-    echo "pimpmykali repository cloned successfully."
-    echo ""
-    echo "Running pimpmykali script..."
-    cd pimpmykali && ./pimpmykali.sh || {
-        echo "Failed to run pimpmykali script. Please check for errors and try again."
-        exit 1
-    }
-    echo ""
-    echo "Install and customization complete."
+    cd /home/kali || exit
+    rm -rf pimpmykali
+    cleanup
 }
 
-# Main function
+# Quit function
+reboot_func() {
+    read -p "Would you like to reboot now? (y/N): " reboot_choice
+    [[ $reboot_choice =~ ^[Yy]$ ]] && reboot || echo "Exiting script without rebooting"
+}
+
+# Display TUI menu
+display_menu() {
+    while true; do
+        read -p "U: Update, I: Headless, D: Desktop, A: All Tools, C: Change Repos, Q: Quit: " choice
+        case $choice in
+        U | u) update_kali ;;
+        I | i) install_headless ;;
+        D | d) install_desktop_default ;;
+        A | a) install_all_pimp ;;
+        C | c) change_repos ;;
+        Q | q) reboot_func && exit 0 ;;
+        *) echo "Invalid. Retry." ;;
+        esac
+    done
+}
+
 main() {
     check_dependencies
     display_menu
